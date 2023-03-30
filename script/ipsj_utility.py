03cf0229efaf86ed925ab2db9039f3667ad13255
kazumakano <kazumakano@outlook.jp>
2023-03-30 22:31:53 +0900

    update figure style

--- script/ipsj_utility.py
+++ script/ipsj_utility.py
@@ -60,7 +60,7 @@ def create_acc_distrib_figure(inertial_val_dict: dict[str, np.ndarray], pos_dict
 @_ipsj_rcparams
 def create_course_figure(pos_dict: dict[str, np.ndarray], quat_dict: dict[str, np.ndarray], result_file_name: Optional[str] = None) -> None:
     fig, axes = plt.subplots(ncols=3, figsize=(12, 4), dpi=1200)
-    fig.subplots_adjust(left=0.05, bottom=0.2, right=0.95, wspace=0.2)
+    fig.subplots_adjust(left=0.05, bottom=0.2, right=0.95, wspace=0.15)
 
     for i, k in enumerate(pos_dict.keys()):
         direct = np.deg2rad(util._quat2direct(quat_dict[k][::400]))
@@ -69,6 +69,8 @@ def create_course_figure(pos_dict: dict[str, np.ndarray], quat_dict: dict[str, n
         axes[i].quiver(pos_dict[k][::400, 0], pos_dict[k][::400, 1], np.cos(direct), np.sin(direct), np.arange(len(pos_dict[k]), step=400), scale=32, width=0.004)
         axes[i].set_title(f"({('a', 'b', 'c')[i]}) {k.capitalize()}", y=-0.3)
         axes[i].set_xlabel("Position [m]")
+        axes[i].set_xticklabels(axes[i].get_xticklabels(), fontsize=8)
+        axes[i].set_yticklabels(axes[i].get_yticklabels(), fontsize=8)
         axes[i].tick_params(color="gray", length=2, width=0.8)
     axes[0].set_ylabel("Position [m]")
 
@@ -86,7 +88,6 @@ def create_sync_figure(acc: np.ndarray, height: np.ndarray, inertial_jump_idxes:
 
     fig, axes = plt.subplots(nrows=2, figsize=(6, 4), dpi=1200)
     fig.align_ylabels()
-    fig.subplots_adjust(hspace=0.3)
 
     acc_margin = 0.15 * (inertial_jump_idxes[1] - inertial_jump_idxes[0])
     acc_lim = (inertial_jump_idxes[0] - acc_margin, inertial_jump_idxes[1] + acc_margin)
@@ -97,14 +98,18 @@ def create_sync_figure(acc: np.ndarray, height: np.ndarray, inertial_jump_idxes:
 
     acc_lim = (max(math.floor(acc_lim[0]), 0), min(math.ceil(acc_lim[1]), len(acc_norm)))
     axes[0].plot(range(*acc_lim), acc_norm[acc_lim[0]:acc_lim[1]], linewidth=0.8)
-    axes[0].scatter(inertial_jump_idxes, acc_norm[inertial_jump_idxes], s=8, c="tab:orange")
+    axes[0].scatter(inertial_jump_idxes, acc_norm[inertial_jump_idxes], s=16, c="tab:orange")
+    axes[0].set_xticklabels(axes[0].get_xticklabels(), fontsize=8)
     axes[0].set_ylabel("Acceleration norm [G]")
+    axes[0].set_yticklabels(axes[0].get_yticklabels(), fontsize=8)
     axes[0].tick_params(color="gray", length=2, width=0.8)
     height_lim = (max(math.floor(height_lim[0]), 0), min(math.ceil(height_lim[1]), len(height)))
     axes[1].plot(range(*height_lim), height[height_lim[0]:height_lim[1]], linewidth=0.8)
-    axes[1].scatter(topcon_jump_idxes, height[topcon_jump_idxes], s=8, c="tab:orange")
+    axes[1].scatter(topcon_jump_idxes, height[topcon_jump_idxes], s=16, c="tab:orange")
     axes[1].set_xlabel("Sample index")
+    axes[1].set_xticklabels(axes[1].get_xticklabels(), fontsize=8)
     axes[1].set_ylabel("Height [m]")
+    axes[1].set_yticklabels(axes[1].get_yticklabels(), fontsize=8)
     axes[1].tick_params(color="gray", length=2, width=0.8)
 
     if result_file_name is not None:
